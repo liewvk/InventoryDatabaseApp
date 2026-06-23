3937a2ccf607b09d310274209c9844e51b18da42
liewvk <voonkiong@gamil.com>
2026-06-23 20:10:12 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace InventoryDatabaseApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblUnitPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblSeacrh = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(208, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(318, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Inventory Database App";
+            this.lblTitle.UseCompatibleTextRendering = true;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(106, 83);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(155, 25);
+            this.lblProductName.TabIndex = 1;
+            this.lblProductName.Text = "Product Name:";
+            this.lblProductName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.Location = new System.Drawing.Point(106, 128);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(107, 25);
+            this.lblCategory.TabIndex = 1;
+            this.lblCategory.Text = "Category:";
+            this.lblCategory.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblUnitPrice
+            // 
+            this.lblUnitPrice.AutoSize = true;
+            this.lblUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnitPrice.Location = new System.Drawing.Point(106, 169);
+            this.lblUnitPrice.Name = "lblUnitPrice";
+            this.lblUnitPrice.Size = new System.Drawing.Size(112, 25);
+            this.lblUnitPrice.TabIndex = 1;
+            this.lblUnitPrice.Text = "Unit Price:";
+            this.lblUnitPrice.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(106, 207);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(100, 25);
+            this.lblQuantity.TabIndex = 1;
+            this.lblQuantity.Text = "Quantity:";
+            this.lblQuantity.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblSeacrh
+            // 
+            this.lblSeacrh.AutoSize = true;
+            this.lblSeacrh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeacrh.Location = new System.Drawing.Point(106, 248);
+            this.lblSeacrh.Name = "lblSeacrh";
+            this.lblSeacrh.Size = new System.Drawing.Size(88, 25);
+            this.lblSeacrh.TabIndex = 1;
+            this.lblSeacrh.Text = "Search:";
+            this.lblSeacrh.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductName.Location = new System.Drawing.Point(334, 85);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(291, 30);
+            this.txtProductName.TabIndex = 2;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategory.Location = new System.Drawing.Point(334, 128);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(291, 30);
+            this.txtCategory.TabIndex = 2;
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(334, 169);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(291, 30);
+            this.txtUnitPrice.TabIndex = 2;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(334, 207);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(291, 30);
+            this.txtQuantity.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(334, 248);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(291, 30);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(61, 303);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 47);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(164, 303);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(115, 47);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(285, 306);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 40);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(427, 306);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(114, 41);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(547, 307);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(120, 40);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(673, 310);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 35);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(61, 356);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.Size = new System.Drawing.Size(685, 158);
+            this.dgvProducts.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.lblSeacrh);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblUnitPrice);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Inventory Database App";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblUnitPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblSeacrh;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvProducts;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace InventoryDatabaseApp
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString =
+    @"Data Source=(localdb)\MSSQLLocalDB;
+      Initial Catalog=InventoryDB;
+      Integrated Security=True;";
+        private void LoadProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql =
+                    "SELECT ProductId, ProductName, Category, UnitPrice, Quantity, " +
+                    "(UnitPrice * Quantity) AS StockValue FROM Products";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+                dgvProducts.DataSource = table;
+            }
+        }
+        private int selectedProductId = 0;
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvProducts.Rows[e.RowIndex];
+
+                selectedProductId = Convert.ToInt32(row.Cells["ProductId"].Value);
+                txtProductName.Text = row.Cells["ProductName"].Value.ToString();
+                txtCategory.Text = row.Cells["Category"].Value.ToString();
+                txtUnitPrice.Text = row.Cells["UnitPrice"].Value.ToString();
+                txtQuantity.Text = row.Cells["Quantity"].Value.ToString();
+            }
+        }
+        private void ClearInput()
+        {
+            selectedProductId = 0;
+            txtProductName.Clear();
+            txtCategory.Clear();
+            txtUnitPrice.Clear();
+            txtQuantity.Clear();
+            txtProductName.Focus();
+        }
+        private void SearchProducts()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string sql =
+                    "SELECT ProductId, ProductName, Category, UnitPrice, Quantity, " +
+                    "(UnitPrice * Quantity) AS StockValue FROM Products " +
+                    "WHERE ProductName LIKE @Search OR Category LIKE @Search";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+
+                adapter.SelectCommand.Parameters.AddWithValue(
+                    "@Search", "%" + txtSearch.Text.Trim() + "%");
+
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+                dgvProducts.DataSource = table;
+            }
+        }
+
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+            LoadProducts();
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            LoadProducts();
+
+        }
+
+
+private void btnAdd_Click(object sender, EventArgs e)
+
+        {
+           
+            string name = txtProductName.Text.Trim();
+            string category = txtCategory.Text.Trim();
+
+            bool okPrice = decimal.TryParse(txtUnitPrice.Text.Trim(), out decimal price);
+            bool okQuantity = int.TryParse(txtQuantity.Text.Trim(), out int quantity);
+
+            
+
+            if (name == "" || category == "" || !okPrice || !okQuantity ||
+                price <= 0 || quantity < 0)
+            {
+                MessageBox.Show("Please enter valid product details.");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql =
+                    "INSERT INTO Products (ProductName, Category, UnitPrice, Quantity) " +
+                    "VALUES (@ProductName, @Category, @UnitPrice, @Quantity)";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductName", name);
+                    command.Parameters.AddWithValue("@Category", category);
+                    command.Parameters.AddWithValue("@UnitPrice", price);
+                    command.Parameters.AddWithValue("@Quantity", quantity);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Product added successfully.");
+
+            LoadProducts();
+            ClearInput();
+        }
+
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedProductId == 0)
+            {
+                MessageBox.Show("Please select a product to update.");
+                return;
+            }
+
+            string name = txtProductName.Text.Trim();
+            string category = txtCategory.Text.Trim();
+
+            bool okPrice = decimal.TryParse(txtUnitPrice.Text, out decimal price);
+            bool okQuantity = int.TryParse(txtQuantity.Text, out int quantity);
+
+            if (name == "" || category == "" || !okPrice || !okQuantity ||
+                price <= 0 || quantity < 0)
+            {
+                MessageBox.Show("Please enter valid product details.");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string sql =
+                    "UPDATE Products SET ProductName=@ProductName, Category=@Category, " +
+                    "UnitPrice=@UnitPrice, Quantity=@Quantity " +
+                    "WHERE ProductId=@ProductId";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                    command.Parameters.AddWithValue("@ProductName", name);
+                    command.Parameters.AddWithValue("@Category", category);
+                    command.Parameters.AddWithValue("@UnitPrice", price);
+                    command.Parameters.AddWithValue("@Quantity", quantity);
+                    command.Parameters.AddWithValue("@ProductId", selectedProductId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Product updated successfully.");
+
+                    LoadProducts();
+                    ClearInput();
+                }
+            }
+        }
+       
+
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedProductId == 0)
+            {
+                MessageBox.Show("Please select a product to delete.");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Delete selected product?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result != DialogResult.Yes)
+            {
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string sql = "DELETE FROM Products WHERE ProductId=@ProductId";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductId", selectedProductId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Product deleted successfully.");
+
+                    LoadProducts();
+                    ClearInput();
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchProducts();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+        }
+    }
+}
+
+            
+
+        
+
+                
+                
+            
+        
+    
